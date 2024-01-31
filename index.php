61b5dc769a2849e547f30cc36ac01fa722c4af2a
ThomasNoSimp <thurawaizin.conceptx@gmail.com>
2024-01-31 21:50:19 +0630

    Update PHP script.

--- index.php
+++ index.php
@@ -6,55 +6,41 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="src/styles/index.css">
+    <link rel="icon" href="src/images/book.png">
     <title>E-Book</title>
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-lg-3">
-                <div class="card card_1">
-                    <div class="card-body">
-                        <h5 class="card-title"><img src="src/images/book.png" alt="Book"></h5>
-                        <button class="btn btn-secondary read-btn">Read</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="card card_2">
-                    <div class="card-body">
-                        <h5 class="card-title"><img src="src/images/book.png" alt="Book"></h5>
-                        <button class="btn btn-secondary read-btn">Read</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="card card_3">
-                    <div class="card-body">
-                        <h5 class="card-title"><img src="src/images/book.png" alt="Book"></h5>
-                        <button class="btn btn-secondary read-btn">Read</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="card card_4">
-                    <div class="card-body">
-                        <h5 class="card-title"><img src="src/images/book.png" alt="Book"></h5>
-                        <button class="btn btn-secondary read-btn">Read</button>
-                    </div>
-                </div>
+            <div class="col-lg-12">
+                <h1 class="text-center">E-Book</h1>
             </div>
         </div>
-        <div class="row">
+        <div class="row mt-5">
+            <?php
+            // Define an array of PDF file paths
+            $pdf_files = array(
+                'src/Books/SharTu_CatWorld.pdf',
+                'src/Books/SharTu_CaWold.pdf',
+                'src/Books/SharTu_Capdf',
+                'src/ads/Books.pdf'
+                //
+            );
 
-        </div>
-        <div class="row">
-
-        </div>
-        <div class="row">
-
-        </div>
-        <div class="row">
-
+            // Loop through each PDF file
+            foreach ($pdf_files as $key => $pdf_file) {
+                ?>
+                <div class="col-lg-3">
+                    <div class="card card_<?php echo $key + 1; ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><img src="src/Books/Book-Images/SharTu_CatWorld.jpeg" alt="Book"></h5>
+                            <a href="<?php echo $pdf_file; ?>" target="_blank" class="btn btn-secondary read-btn">Read</a>
+                        </div>
+                    </div>
+                </div>
+                <?php
+            }
+            ?>
         </div>
     </div>
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Get all cards
-    let cards = document.querySelectorAll('.card');
 
-    // Iterate over each card
-    cards.forEach(function(card) {
-        // Add click event listener to the card
-        card.addEventListener('click', function() {
-            window.location.href = '../../phpinfo.php';
-        });
 
-        // Get the button inside the card
-        let button = card.querySelector('.read-btn');
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Get all cards
+//     let cards = document.querySelectorAll('.card');
 
-        // Add click event listener to the button
-        button.addEventListener('click', function(event) {
-            event.stopPropagation(); // Prevent the click event on the card from triggering
-            window.location.href = '../../phpinfo.php';
-        });
-    });
-});
+//     // Iterate over each card
+//     cards.forEach(function(card) {
+//         // Add click event listener to the card
+//         card.addEventListener('click', function() {
+//             window.location.href = '../../phpinfo.php';
+//         });
+
+//         // Get the button inside the card
+//         let button = card.querySelector('.read-btn');
+
+//         // Add click event listener to the button
+//         button.addEventListener('click', function(event) {
+//             event.stopPropagation(); // Prevent the click event on the card from triggering
+//             window.location.href = '../../phpinfo.php';
+//         });
+//     });
+// });
